fe140d5cdb0915978f6e7903f8589da8fd66cb15
coryunderwood <coryunderwood@users.noreply.github.com>
2014-10-10 17:50:21 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,22 +1,27 @@
-$(document).ready(function(){
-	$('form#entertask').submit(function(event){
+//Because this file loads after the elements are in the DOM, you do not need to wait for DOM Ready.
+
+	/*Because you use ids on the elements, do you not need to also use the element. 
+	  ids are unquie on the page by nature, maybe consider a id for the button itself.*/
+	$('.btn-primary').submit(function(event){
 		event.preventDefault();
 
-		var inputTask = $('input#addtask').val();
+		var inputTask = $('#addtask').val();
 
 		if(inputTask !== ""){
-			$('ol#tasks').append('<li class="todo">' + inputTask + '</li>');
+			$('#tasks').append('<li class="todo">' + inputTask + '</li>');
 			$('.form-group').removeClass('has-error');
-			$('form#entertask')[0].reset();
+			$('#entertask')[0].reset();
 		}else{
 			alert("Please enter a task");
 			$('.form-group').addClass('has-error');
 		}
-		
-		$('li.todo').hover().click(function(){
-			$(this).removeClass('todo').addClass('done');
-		});
 	});
-});
+	
+	/*This handler has to happen outside of the click handler,
+	  or you will cause the event to be attached / fire multiple times.*/
+	
+	$('li.todo').hover().click(function(){
+		$(this).removeClass('todo').addClass('done');
+	});
 
 //need to add delete task code
